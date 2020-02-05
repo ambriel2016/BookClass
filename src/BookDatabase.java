2843341473e1ae74c7fadc378c4b8ae7c761e369
GBTC440002UR <GBTC440002UR@MC.MONTGOMERYCOLLEGE.EDU>
2020-02-05 16:52:47 -0500

    Book Database class added

--- src/BookDatabase.java
+++ src/BookDatabase.java
@@ -1,4 +1,55 @@
-package PACKAGE_NAME;
-
 public class BookDatabase {
+    private String SKU;
+    private String title;
+    private String author;
+    private String description;
+    private double price;
+    private Book book;
+
+    public BookDatabase(){
+
+    }
+    public BookDatabase(String SKU, String title, String author, String description, double price) {
+        this.SKU = SKU;
+        this.title = title;
+        this.author = author;
+        this.description = description;
+        this.price = price;
+    }
+    public String getSKU(){
+        return this.SKU;
+    }
+    public void setSKU(String SKU){
+        this.SKU = SKU;
+    }
+    public String getTitle(){
+        return this.title;
+    }
+    public void setTitle(String title){
+        this.title = title;
+    }
+    public String getAuthor(){
+        return this.author;
+    }
+    public void setAuthor(String author){
+        this.author = author;
+    }
+    public String getDescription(){
+        return this.description;
+    }
+    public void setDescription(String description){
+        this.description = description;
+    }
+    public double price(){
+        return this.price;
+    }
+    public void setPrice(double price){
+        this.price = price;
+    }
+    public Book getBook(){
+        return this.book;
+    }
+    public void setBook(Book book){
+        this.book = book;
+    }
 }
